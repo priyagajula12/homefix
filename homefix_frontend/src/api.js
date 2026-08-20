9a2380fe7fc427f30f9fd6706576838aec52b62d
gajula21 <vivekgajula2004@gmail.com>
2026-08-21 01:23:53 +0530

    Update API base URL to live Render backend

--- homefix_frontend/src/api.js
+++ homefix_frontend/src/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-const api = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api/',
-});
+const api = axios.create {
+    baseURL: 'https://homefix-backend-l33n.onrender.com/api/',
+};
 
 // The Interceptor: Automatically attaches your security token to every request
 api.interceptors.request.use(
