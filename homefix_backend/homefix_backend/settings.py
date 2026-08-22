ab514f7d1779fc668335698e66ae6332c0fb1993
gajula21 <vivekgajula2004@gmail.com>
2026-08-22 07:53:08 +0530

    Rename STATIC_ROOT to assets to force fresh CSS copy

--- homefix_backend/homefix_backend/settings.py
+++ homefix_backend/homefix_backend/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 # Email
 MAILERS = {
